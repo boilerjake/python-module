cba0e726ad615d5bcaeae428b922bbd8e269d18c
Jake Ledoux <contactjakeledoux@gmail.com>
2021-01-31 10:30:08 -0500

    More fixes

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,7 +2,7 @@ from .context import <<[.cus.package.name.python]>>
 import pytest
 
 class TestModels:
-    def test_sample_class():
+    def test_sample_class(self):
         assert 'pigs' != 'flying'
         sample_instance = <<[.cus.package.name.python]>>.SampleClass
         assert sample_instance is not None
